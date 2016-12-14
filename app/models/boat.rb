f4e52c9bae79520d82ed0542a3aa8f6693f79584
Jing Yu <jing@MacBook-Air.local>
2016-12-13 23:18:34 -0500

    boats

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,37 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    all.limit(5)
+  end
+
+  def self.dinghy
+    all.where("length < 20")
+  end
+
+  def self.ship
+    all.where("length >= 20")
+  end
+
+  def self.last_three_alphabetically
+    all.order("name desc").limit(3)
+  end
+
+  def self.without_a_captain
+    all.where(:captain_id=>nil)
+  end
+
+  def self.sailboats
+    Boat.includes(:classifications).where('classifications.name = ?', "Sailboat")
+  end
+
+  def self.with_three_classifications
+    Boat.joins(:boat_classifications).group("boat_classifications.boat_id").having("count(*) = 3")
+  end
+
+  def self.longest
+    Boat.order('length DESC').first
+  end
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,24 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    Captain.joins(boats: :classifications).where('classifications.name = ?', "Catamaran")
+  end
+
+  def self.sailors
+    Captain.joins(boats: :classifications).where('classifications.name = ?', "Sailboat").distinct
+  end
+
+  def self.motorboaters
+    Captain.joins(boats: :classifications).where('classifications.name = ?', "Motorboat").distinct
+  end
+
+  def self.talented_seamen
+    Captain.where('id IN (?)', self.sailors.pluck(:id) & self.motorboaters.pluck(:id))
+  end
+
+  def self.non_sailors
+    Captain.where.not('id IN (?)', self.sailors.pluck(:id))
+
+  end
 end
